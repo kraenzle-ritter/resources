d654716355f3b705dfd92e328a093d55625680e7
andreas_kraenzle <kraenzle@k-r.ch>
2021-02-25 09:46:50 +0100

    delete doublets

--- src/Console/Commands/ResourcesFetch.php
+++ src/Console/Commands/ResourcesFetch.php
@@ -190,7 +190,7 @@ class ResourcesFetch extends Command
                 INSERT INTO resources SELECT * FROM tmp;
                 DROP TABLE tmp;';
         $count2 =  DB::table('resources')->count();
-        $this->success('Deleted ' $count-$count . ' resourceso');
+        $this->success('Deleted ' . $count-$count2 . ' resourceso');
         DB::statement(DB::raw($select));
     }
 }
